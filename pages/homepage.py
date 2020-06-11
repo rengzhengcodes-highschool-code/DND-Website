cf0c023a395b35c4c22c30986e7de2479135f358
rengzhengcodes <64385210+rengzhengcodes@users.noreply.github.com>
2020-06-11 11:38:12 -0400

    Add files via upload

--- pages/homepage.py
+++ pages/homepage.py
@@ -3,7 +3,16 @@ import cgi
 import codecs
 import requests
 def html_page():
-    print('Content-type:text/html\n\n' + 
-    codecs.open('template.html','r', 'utf-8').read().format('homepage')
-    )
-html_page()
+    page = codecs.open('template.html', 'r', 'utf-8').read()
+    body_code = body_format()
+    page.format(title = 'homepage', body = body_code)
+    print('Content-type:text/html\n\n' + page)
+    print('<p>' + body_code + '<p>')
+
+def body_format():
+    response = requests.get('https://api.open5e.com/spells/?page=1')
+    return str(response)
+try:
+    html_page()
+except:
+    print(cgi.print_exception())
