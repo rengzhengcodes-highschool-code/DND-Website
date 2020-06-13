5fc0eb9c830038914fcba4446cdf2485b923d9f2
rengzhengcodes <64385210+rengzhengcodes@users.noreply.github.com>
2020-06-13 16:46:09 -0400

    Add files via upload

--- pages/index.py
+++ pages/index.py
@@ -9,27 +9,10 @@ def get_spell(): #retrieves name of a random spell
     with open('dependencies/index/slugs.txt') as file:
         spells = list(file)
         spell = (spells[random.randint(0, len(spells) - 1)]).rstrip('\n')
-        file.close()
         return spell
 
 
-def spell_api_import():
-    spell = get_spell()
-    response = requests.get('https://api.open5e.com/spells/?slug__iexact={spell}'.format(spell=spell))
-    data = response.content.decode('utf-8')
-    data = str(data)
-    data = data[data.find('[')+3: data.rfind(']')-2] #removes extraneous characters
-    level_int_location = data.find('"level_int"') + 12 #this is a data point that isnt stored as a string in the api, which messes up the splitter function. We're adding a character in so it splits at the desired location.
-    data = data[: level_int_location] + data[level_int_location] + '"' + data[level_int_location + 1:] #Adds character so split() can process the split correctly.
-    data = list(data.split('","'))
-    for i in range(len(data)):
-        data[i] = data[i].replace('"', '', 2) #removes extraneous "
-    spell_attributes = dict()
-    for attribute in data.copy():
-        colon_separator_location = attribute.find(':')
-        category, value = attribute[0:colon_separator_location], attribute[colon_separator_location + 1:] #sometimes colons are used besides demarcating a split between category and information, necessitating we split only on this colon.
-        spell_attributes[category] = value
-    return spell_attributes
+spell_api_import = lambda spell: requests.get('https://api.open5e.com/spells/?slug__iexact={spell}'.format(spell=spell)).json()['results'][0]
 
 
 def markdown_to_html_marker(string, markdown_marker, html_marker):
@@ -54,52 +37,33 @@ def python_to_html_marker(string, python_marker, html_marker):
 
 
 def python_to_html(string):
-    string = python_to_html_marker(string, '\\n', '<br>')
+    string = python_to_html_marker(string, '\n', '<br>')
     string = python_to_html_marker(string, '\"', '"')#this isn't actually inherently a python marker but a work around to removing the " from earlier
     return string
 
 
-def attribute_html_format(info, header=''):
-    info = python_to_html(info)
-    info = markdown_to_html(info)
-    attribute_format = '<b>{header}</b>{information} <br>\n'
-    attribute_format = attribute_format.format(header = header, information = info)
-    return attribute_format
-
-
 def spell_html_format():
-    spell = spell_api_import()
-    container = '''<div class = "header">
-                        <h2 class = "heading">Learn a New Spell: {name}</h2>
-                    </div>
-                    <div class = "text">
-                        <p>{spelldesc}<p>
-                        <p style = "font-size: 24px;"><a href='{license}'>License to SRD Material</a></p>
-                    </div>'''
+    spell = spell_api_import(get_spell())
+    container = codecs.open('dependencies/index/spell.html', 'r', 'utf-8').read()
     name = spell['name']
-    spell_restrictions = '<i>{level} {school}</i> | {classes} | {archetypes}'.format(level = spell['level'], school = spell['school'],classes = spell['dnd_class'], archetypes = spell['archetype']) #puts all the spell restrictions in 1 string
-    spelldesc = attribute_html_format(spell_restrictions) + '<br>' #formats spell restrictions for html
-    spelldesc += attribute_html_format(spell['range'], 'Range: ')
-    spelldesc += attribute_html_format(spell['duration'], 'Duration: ')
     if spell['material']: #combines components and flavortext materials line if materials line exists
         spell_components = '{components} ({materials})'.format(components = spell['components'], materials = spell['material'])
     else:
         spell_components = spell['components']
-    spelldesc += attribute_html_format(spell_components, 'Components: ')
-    spell_properties = 'Concentration: {concentration} | Ritual: {ritual}'.format(concentration = spell['concentration'], ritual = spell['ritual'])
-    spelldesc += attribute_html_format(spell_properties)
-    spelldesc += '<br>' + attribute_html_format(spell['desc']) #adds spell flavortext.
     if spell['higher_level']: #adds what happens at a higher level, if anything
-        spelldesc += '<br>' + attribute_html_format(spell['higher_level'], 'At Higher Levels: ')
-    spelldesc += '<br>' + attribute_html_format(spell['page'], 'Source: ')
-    container = container.format(name = name, spelldesc = spelldesc, license = spell['document__license_url'])
+        higher_level = '<br> <b>At Higher Levels: </b>' + spell['higher_level']
+    else:
+        higher_level = ''
+    container = container.format(name = name, level = spell['level'], school = spell['school'], classes = spell['dnd_class'], archetypes = spell['archetype'], 
+    range=spell['range'], duration=spell['duration'], components=spell_components, concentration=spell['concentration'], ritual=spell['ritual'], spelldesc = spell['desc'], 
+    higher_level=higher_level, source=spell['page'], license = spell['document__license_url'])
+    container = markdown_to_html(python_to_html(container))
     return container
 
 
 def main():
-    page = codecs.open('dependencies/index/index.html', 'r', 'utf-8').read()
     spell = spell_html_format()
-    page = 'Content-type:text/html\n\n' + page.format(title='homepage', body=spell)
+    page = 'Content-type:text/html\n\n' + codecs.open('dependencies/index/index.html', 'r', 'utf-8').read().format(title='homepage', body=spell)
     print(page)
 
 
--- pages/questionaire.py
+++ pages/questionaire.py
@@ -4,72 +4,31 @@ import cgi
 import codecs
 
 
-def getData(x): #imports cgi formdata
-    form = cgi.FieldStorage()
-    if form.getvalue(x):
-        subject = form.getvalue(x)
-    else:
-        subject = "none"
-    return subject
-
-
-dependencies = 'dependencies/questionaire/' #dependency relative path
+get_data = lambda name: cgi.FieldStorage().getvalue(name)
+get_dependency = lambda filename: codecs.open('dependencies/questionaire/' + filename, 'r', 'utf-8').read()
 
 
 def main():
-    print('Content-type:text/html\n\n') 
-    template = codecs.open('template.html', 'r', 'utf-8')
-    class_type = getData("class_type")
-    dnd_class = getData("class1")
-    race = getData("race")
-    background = getData("background")
-    show_menu = True
-    page = template.read().format(title='Questionaire', body='''
-    <center>
-        <div class = "text">
-            <form method='get' action = 'questionaire.py'>
-                {}
-            </form>
-        </div>
-    </center>
-    ''', update='6/12/2020 by M. Rudin-Aulenbach')
-    if background != "none": #gives results and enables a reset
-        page = page.format('''<h1>Congratulations! You used to be a {background} but now you are a {race} {dnd_class}!</h1>
-                 <form action = 'questionaire.py'>
-                    <input type='reset' value='reset'/>'''.format(background=background, race=race, dnd_class=dnd_class))
-        race = "none"
-        dnd_class = "none"
-        print(page)
-        show_menu = False
-        show_menu = False
-    elif race != "none": #asks questions to determine your dnd race
-        sub = codecs.open(dependencies + 'what_background_are_you.html', 'r', 'utf-8').read()
-        page = page.format('''<div class = "text">
-                                <input type='hidden' NAME = 'class_type' VALUE="{class_type}"/>
-                                <input type='hidden' NAME = 'class1' VALUE="{dnd_class}"/>
-                                <input type='hidden' NAME = 'race' VALUE="{race}"/>
-                                {sub}'''.format(class_type = class_type, dnd_class = dnd_class, race = race, sub = sub))
-        print(page)
-        show_menu = False
-    elif dnd_class != "none": #asks questions to determine your class
-        sub=codecs.open(dependencies + 'what_race_are_you.html', 'r', 'utf-8')
-        page = page.format('''<input type='hidden' NAME = 'class_type' VALUE="{class_type}"/>
-                    <input type='hidden' NAME = 'class1' VALUE="{dnd_class}"/>'''.format(class_type = class_type, dnd_class = dnd_class) + sub.read())
-        print(page)
-        show_menu = False
-    elif class_type == "martial": #martial character questions
-        sub=codecs.open(dependencies + 'martial_class.html', 'r', 'utf-8')
-    elif class_type == "both": #both character questions
-        sub=codecs.open(dependencies + 'both.html', 'r', 'utf-8')
-    elif class_type == "spellcaster": #spellcaster questions
-        sub=codecs.open(dependencies + 'spell_class.html', 'r', 'utf-8')
-    if class_type != "none" and dnd_class == "none" and background == "none":
-        page = page.format(''' <input type='hidden' NAME = 'class_type' VALUE="{class_type}"/>'''.format(class_type=class_type) + sub.read())
-        print(page)
-        show_menu = False
-    if show_menu:
-        page = page.format(codecs.open(dependencies + 'what_class_are_you_survey.html', 'r', 'utf-8').read()) #start page
-        print(page)
+    page = 'Content-type:text/html\n\n' + get_dependency('form_template.html')
+    form_names = ['class_type', 'dnd_class', 'race', 'background']
+    form_values = dict()
+    for name in form_names: form_values[name] = get_data(name)
+    if form_values['background']: #gives results and enables a reset
+        sub = get_dependency('result.html').format(background = form_values['background'], race = form_values['race'], dnd_class = form_values['dnd_class'])
+    elif form_values['race']: #asks questions to determine your dnd race
+        sub = get_dependency('what_background_are_you.html').format(dnd_class = form_values['dnd_class'], race = form_values['race'])
+    elif form_values['dnd_class']: #asks questions to determine your class
+        sub = get_dependency('what_race_are_you.html').format(dnd_class = form_values['dnd_class'])
+    elif form_values['class_type'] == "martial": #martial character questions
+        sub = get_dependency('martial_class.html')
+    elif form_values['class_type'] == "both": #both character questions
+        sub = get_dependency('both.html')
+    elif form_values['class_type'] == "spellcaster": #spellcaster questions
+        sub = get_dependency('spell_class.html')
+    else:
+        sub = get_dependency('what_class_are_you_survey.html') #start page
+    print(page.format(sub))
+
 
 if __name__ == '__main__':
     try:
